d6ec50065746d423add0f82fe508c189151857ba
mzajac <matzaj101@gmail.com>
2018-11-30 17:54:45 +0100

    test->masterbranch

--- RepositoryUsage/Models/ErrorViewModel.cs
+++ RepositoryUsage/Models/ErrorViewModel.cs
@@ -7,5 +7,6 @@ namespace RepositoryUsage.Models
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public string TestProp2 { get; set; }
     }
 }
